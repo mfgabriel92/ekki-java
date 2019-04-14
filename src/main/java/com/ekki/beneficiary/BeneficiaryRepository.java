d81cd847efc3b8551c098e00d8450fd8b292d48b
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2019-04-13 22:09:02 -0300

    Remove unused import

--- src/main/java/com/ekki/beneficiary/BeneficiaryRepository.java
+++ src/main/java/com/ekki/beneficiary/BeneficiaryRepository.java
@@ -1,10 +1,9 @@
 package com.ekki.beneficiary;
 
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public interface BeneficiaryRepository extends CrudRepository<Beneficiary, Integer> {
